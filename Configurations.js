acdfa692ab5a094e878db95c606d9b8a2de194f8
BlackHat <123658198+Mubaraq0@users.noreply.github.com>
2024-05-19 18:51:38 +0100

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "2348022444782";   // You can replace this number with yours //
 }
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
-global.sessionId = process.env.SESSION_ID || "ok";
+global.mongodb = process.env.MONGODB || "mongodb+srv://koolmoney62:BmROu85yT00XuKK9@bluee.bjvm3me.mongodb.net/?retryWrites=true&w=majority";
+global.sessionId = process.env.SESSION_ID || "babynaomi";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Atlas Bot`;
